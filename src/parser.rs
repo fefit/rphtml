5a26d57c1c90f6806581b32bce0a5ca74cff8f4e
ganmin <ganmin@360.cn>
2020-09-08 18:46:43 +0800

    change parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,6 @@
+use std::cell::RefCell;
 use std::collections::LinkedList;
 use std::error::Error;
-
 const TAG_BEGIN_CHAR: char = '<';
 const TAG_END_CHAR: char = '>';
 /**
@@ -8,10 +8,12 @@ const TAG_END_CHAR: char = '>';
  * HTML: for html
  * XML: for xml
  */
+#[derive(PartialEq)]
 pub enum ParserType {
   HTML,
   XML,
 }
+
 pub enum NodeType {
   Comment,      // 注释节点
   HTMLDoctype,  // HTML的Doctype声明
@@ -21,30 +23,25 @@ pub enum NodeType {
   Text,         // 文本节点
   AttrKey,      // 属性名
   AttrValue,    // 属性值
-  Entity,       //实体转义字符
+  Entity,       // 实体转义字符
   AbstractRoot, // 根节点、用于文档开头等
 }
 pub enum CodeTypeIn {
-  BOF,
-  TagBegin,
-  TagEnd,
-  StartTagBegin,
-  StartTagEnd,
-  EndTagBegin,
-  EndTagEnd,
-  CommentTagBegin,
-  CommentBegin,
-  DoubleQuoteAttr,
-  SingleQuoteAttr,
-  TextNode,
-  TextEmpty,
-  TextEntity,
+  BOF,             // 文本开头
+  TagBegin,        // 标签开始
+  Comment,         // 注释
+  CDATA,           // XML CDATA数据
+  DoubleQuoteAttr, // 双引号属性值
+  SingleQuoteAttr, // 单引号属性值
+  UnQuoteAttr,     // 无引号属性值
+  TextNode,        // 文本节点
   EOF,
 }
 
 /**
  * 当前解析行列位置
 */
+#[derive(Copy, Clone)]
 pub struct CodePosAt {
   pub line_no: u32,
   pub col_no: u32,
@@ -69,29 +66,53 @@ impl CodePosAt {
     self.col_no += 1;
   }
 }
+
+/**
+ * Attr 标签属性
+ */
+pub struct Attr {
+  pub key: Option<String>,
+  pub value: Option<String>,
+}
+
+/**
+ * Tag 标签节点信息
+*/
+pub struct Tag {
+  pub name: String,
+  pub namespace: String,
+  pub fullname: String,
+  pub attrs: Vec<Attr>,
+}
+
 /**
  * Node节点
  */
-pub struct Node {
-  pub node_type: NodeType,
-  pub parent: LinkedList<Node>,
-  pub position: CodePosAt,
-  pub content: Vec<char>,
+pub struct Node<'a> {
+  pub node_type: NodeType,               // 节点类型
+  pub start_begin_at: CodePosAt,         // 节点开始标签开始位置
+  pub start_end_at: Option<CodePosAt>,   // 节点开始标签结束位置
+  pub close_begin_at: Option<CodePosAt>, // 节点结束标签开始位置
+  pub close_end_at: CodePosAt,           // 节点结束标签结束位置
+  pub child: Vec<&'a Node<'a>>,          // 子节点、针对标签
+  pub content: Vec<char>,                // 内容部分、针对text文本节点
 }
 /**
  * Doc 文档
  * 解析
 */
-pub struct Doc {
-  code_in: CodeTypeIn,
-  position: CodePosAt,
+pub struct Doc<'a> {
+  code_in: CodeTypeIn, // 当前解析标签位置
+  position: CodePosAt, // 当前解析字符位置
   prev_chars: Vec<char>,
   prev_char: Option<char>,
-  nodes: Vec<Node>,
+  chain_nodes: Vec<RefCell<Node<'a>>>,
+  pub parser_type: ParserType,
+  pub nodes: Vec<Node<'a>>,
 }
-impl Doc {
+impl<'a> Doc<'a> {
   // 创建实例
-  pub fn new() -> Self {
+  pub fn new(parser_type: ParserType) -> Self {
     let current_node = Node {
       node_type: NodeType::AbstractRoot,
       parent: LinkedList::new(),
@@ -103,9 +124,10 @@ impl Doc {
     Doc {
       code_in: CodeTypeIn::BOF,
       position: CodePosAt::begin(),
-      prev_chars: Vec::with_capacity(30),
-      nodes,
       prev_char: None,
+      prev_chars: Vec::with_capacity(30),
+      parser_type,
+      nodes,
     }
   }
   // 读取一行
@@ -118,7 +140,10 @@ impl Doc {
       prev_chars,
       prev_char,
       nodes,
+      parser_type,
+      ..
     } = self;
+    let isHTML = parser_type == &ParserType::HTML;
     // 引入CodeTypeIn里的所有枚举声明
     use CodeTypeIn::*;
     match code_in {
@@ -145,6 +170,10 @@ impl Doc {
       },
       TagBegin => {
         // 遇到开始标签
+        match c {
+          // 如果是html定义的ascii空格
+          space if space.is_ascii_whitespace() => {}
+        }
       }
     }
     Ok(self)
