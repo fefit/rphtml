39cc999d486e2f30c416a6d0d0505d83b867366b
ganmin <ganmin@360.cn>
2020-12-02 18:51:15 +0800

    add more benchmark

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,10 +1,21 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use rphtml::config::ParseOptions;
 use rphtml::parser::*;
 fn parse_doc() {
 	let _ = Doc::parse_file("./cases/full.html", Default::default());
 }
+fn parse_doc_decode_entity() {
+	let _ = Doc::parse_file(
+		"./cases/full.html",
+		ParseOptions {
+			decode_entity: true,
+			..Default::default()
+		},
+	);
+}
 fn criterion_benchmark(c: &mut Criterion) {
-	c.bench_function("parse", |b| b.iter(|| parse_doc()));
+	c.bench_function("parse-default", |b| b.iter(parse_doc));
+	c.bench_function("parse-with-decode", |b| b.iter(parse_doc_decode_entity));
 }
 
 criterion_group!(benches, criterion_benchmark);
